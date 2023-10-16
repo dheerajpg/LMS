b1e2bb4df2b32d90061368132b3ac3f9b65c6df4
Dheeraj <70167116+dheerajpg@users.noreply.github.com>
2023-10-16 16:11:26 +0530

    Teachers layout Update and Error Toast added

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -11,5 +11,5 @@ export const Sidebar = () => {
                 <SidebarRoutes />
             </div>
         </div>
-    )
+    ) 
 }   
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ToastProvider } from '@/components/providers/toaster_provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   )
 }
