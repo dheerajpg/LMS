cdafebaa8dc7436da07639caa7d40b95c6bf7945
Dheeraj <70167116+dheerajpg@users.noreply.github.com>
2023-10-24 00:52:07 +0530

    Added ability to edit course title

--- app/(dashboard)/(routes)/teacher/courses/[courseid]/_components/title_form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseid]/_components/title_form.tsx
@@ -4,6 +4,8 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { Pencil } from "lucide-react";
 
 import {
   Form,
@@ -26,10 +28,38 @@ const FormSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }).max(100, { message: "Keep title short and simple so that it's easy to remember" })
 })
 
-export const TitleForm = ({ intitalData, courseId }: TitleFormProps) => {
+export const TitleForm = ({
+  initialData,
+  courseId
+}: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: initialData
+  })
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    console.log(values)
+  }
   return (
-    <div>
-      Title FOrm
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Title
+        <Button variant="ghost">
+          {isEditing && (
+            <>Cancel</>
+          )}
+          {!isEditing && (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit
+            </>
+          )}
+        </Button>
+      </div>
     </div>
   )
 }
